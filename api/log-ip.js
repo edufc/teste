b9364b02fd392b9dd6e8dd1f0da5673917736201
edufc <eduardo.carvalho@gpnxgroup.com>
2024-08-16 20:39:02 -0300

    first commit

--- api/log-ip.js
+++ api/log-ip.js
@@ -5,32 +5,23 @@ app.use(express.json()); // To parse JSON request bodies
 
 app.set('trust proxy', true); // Trust the proxy to get the correct IPs
 
-// GET method to log and return IP addresses
 app.get('/', (req, res) => {
     const ipv4 = req.ip;
     const ipv6 = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-    console.log('IPv4:', ipv4);
-    console.log('IPv6:', ipv6);
-
-    res.send({
+    res.json({
         message: 'Logged your IP addresses',
         ipv4,
         ipv6
     });
 });
 
-// POST method to log, return IP addresses, and echo back the JSON data sent
 app.post('/', (req, res) => {
     const ipv4 = req.ip;
     const ipv6 = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const data = req.body;
 
-    console.log('IPv4:', ipv4);
-    console.log('IPv6:', ipv6);
-    console.log('Received data:', data);
-
-    res.send({
+    res.json({
         message: 'Logged your IP addresses and received your data',
         ipv4,
         ipv6,
@@ -38,7 +29,5 @@ app.post('/', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export the app as a Vercel function
+module.exports = app;
